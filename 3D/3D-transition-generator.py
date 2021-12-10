b2ed8de5a018b53a8a2606b47f9db792d10b044f
Timdnb <timdnblanken@gmail.com>
2021-12-10 16:16:09 +0100

    Add color gradient

--- 3D/3D-transition-generator.py
+++ 3D/3D-transition-generator.py
@@ -24,7 +24,7 @@ def plot_config(title):
     Contains the configuration for the plots
     """
     line_thickness = 6
-    line_color = "black"
+    line_color = "#000000"
 
     plt.ylim(-0.05, 0.3)
     plt.xlim(0, 0.7)
@@ -35,39 +35,90 @@ def plot_config(title):
     plt.title(title)
     plt.show()
 
+colors = ['#38f523',
+'#38f523',
+'#38f523',
+'#4cf107',
+'#5bed00',
+'#68e900',
+'#73e500',
+'#7ce100',
+'#85dd00',
+'#8dd900',
+'#94d500',
+'#9bd100',
+'#a1cc00',
+'#a7c800',
+'#adc400',
+'#b2bf00',
+'#b8bb00',
+'#bdb600',
+'#c1b100',
+'#c6ac00',
+'#caa700',
+'#cea300',
+'#d29d00',
+'#d59800',
+'#d99300',
+'#dc8e00',
+'#df8800',
+'#e28300',
+'#e57d00',
+'#e77700',
+'#e97100',
+'#eb6b00',
+'#ed6400',
+'#ef5e00',
+'#f05700',
+'#f15000',
+'#f24800',
+'#f34000',
+'#f43600',
+'#f43600',
+'#f42c00',
+'#f41f00',
+'#f41f00',
+'#f40909',
+'#f40909',
+]
+
 
 # Plots without tip, LLT
 plt.figure()
-
+k = 2
 for i in np.arange(-2, 18, 0.5):
     y_lst, XTrtop_lst = grab_values(f"OP_points_no_tip\LLT\MainWing_a={i}0_v=44.00ms.txt", 30, 0)
-    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24))
+    k += 1
+    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24), color=colors[k])
 
 plot_config("Transition lines without tip, LLT")
 
 # Plots without tip, VLM
 plt.figure()
-
+k = 6
 for i in np.arange(-0.5, 14.5, 0.5):
+    k += 1
     y_lst, XTrtop_lst = grab_values(f"OP_points_no_tip\VLM\MainWing_a={i}0_v=44.00ms.txt", 40, 536)
-    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24))
+    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24), color=colors[k])
 
 plot_config("Transition lines without tip, VLM")
 
 # Plots with tip, LLT
 plt.figure()
-
+k = 0
 for i in np.arange(-3, 19, 0.5):
+    k += 1
     y_lst, XTrtop_lst = grab_values(f"OP_points_tip\LLT\MainWing_a={i}0_v=44.00ms.txt", 30, 0)
-    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24))
+    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24), color=colors[k])
 
 plot_config("Transition lines with tip, LLT")
 
 # Plots with tip, VLM
 plt.figure()
-
+k = 3
 for i in np.arange(-3, 14, 0.5):
+    k += 1
     y_lst, XTrtop_lst = grab_values(f"OP_points_tip\VLM\MainWing_a={i}0_v=44.00ms.txt", 76, 1544)
-    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24))
+    plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24), color=colors[k])
 
 plot_config("Transition lines with tip, VLM")
