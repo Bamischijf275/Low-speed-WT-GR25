66e2f3add01aa7b4b8c3e6103719193d3137d001
Timdnb <timdnblanken@gmail.com>
2021-12-10 15:41:24 +0100

    Changes

--- 3D/3D-transition-generator.py
+++ 3D/3D-transition-generator.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
-
 def grab_values(filename, skip_begin, skip_end):
     """
     Function that grabs data from an XFLR5 file
@@ -16,33 +13,33 @@ def grab_values(filename, skip_begin, skip_end):
     # The last values are the actual total values! The sectional values are the lists!
     return y_lst, XTrtop_lst
 
+def plot_config(title):
+    plt.ylim(-0.05, 0.3)
+    plt.xlim(0, 0.7)
+    plt.plot((0,0),(0,0.24),linewidth = 6, color = "black")
+    plt.plot((0,0.64),(0.24,0.24),linewidth = 6,color = "black")
+    plt.plot((0.64,0.64),(0.24,0),linewidth = 6,color = "black")
+    plt.plot((0.64,0),(0.0,0.0),linewidth = 6,color = "black")
+    plt.title(title)
+    plt.show()
+
 # Plots without tip, LLT
 plt.figure()
 
-for i in np.arange(-3,19,0.5):
-    y_lst, XTrtop_lst = grab_values(f"OP_points_tip\LLT\MainWing_a={i}0_v=44.00ms.txt", 30, 0)
-    plt.plot(y_lst, XTrtop_lst)
-    plt.ylim(-0.05, 0.3)
-plt.plot((0,0),(0,0.24),linewidth = 6, color = "black")
-plt.plot((0,0.64),(0.24,0.24),linewidth = 6,color = "black")
-plt.plot((0.64,0.64),(0.24,0),linewidth = 6,color = "black")
-plt.plot((0.64,0),(0.0,0.0),linewidth = 6,color = "black")
-plt.title("Transition lines without tip, LLT")
-plt.show()
+for i in np.arange(-2,18,0.5):
+    y_lst, XTrtop_lst = grab_values(f"OP_points_no_tip\LLT\MainWing_a={i}0_v=44.00ms.txt", 30, 0)
+    plt.plot(y_lst, np.multiply(XTrtop_lst,0.24))
+
+plot_config("Transition lines without tip, LLT")
 
 # Plots without tip, VLM
 plt.figure()
 
-for i in np.arange(-3,19,0.5):
-    y_lst, XTrtop_lst = grab_values(f"OP_points_tip\LLT\MainWing_a={i}0_v=44.00ms.txt", 30, 0)
-    plt.plot(y_lst, XTrtop_lst)
-    plt.ylim(-0.05, 0.3)
-plt.plot((0,0),(0,0.24),linewidth = 6, color = "black")
-plt.plot((0,0.64),(0.24,0.24),linewidth = 6,color = "black")
-plt.plot((0.64,0.64),(0.24,0),linewidth = 6,color = "black")
-plt.plot((0.64,0),(0.0,0.0),linewidth = 6,color = "black")
-plt.title("Transition lines without tip, LLT")
-plt.show()
+for i in np.arange(-0.5,14.5,0.5):
+    y_lst, XTrtop_lst = grab_values(f"OP_points_no_tip\VLM\MainWing_a={i}0_v=44.00ms.txt", 40, 536)
+    plt.plot(y_lst, np.multiply(XTrtop_lst,0.24))
+
+plot_config("Transition lines without tip, VLM")
 
 # Plots with tip, LLT
 plt.figure()
@@ -50,14 +47,8 @@ plt.figure()
 for i in np.arange(-3,19,0.5):
     y_lst, XTrtop_lst = grab_values(f"OP_points_tip\LLT\MainWing_a={i}0_v=44.00ms.txt", 30, 0)
     plt.plot(y_lst, np.multiply(XTrtop_lst,0.24))
-    plt.ylim(-0.05, 0.3)
 
-plt.plot((0,0),(0,0.24),linewidth = 6, color = "black")
-plt.plot((0,0.64),(0.24,0.24),linewidth = 6,color = "black")
-plt.plot((0.64,0.64),(0.24,0),linewidth = 6,color = "black")
-plt.plot((0.64,0),(0.0,0.0),linewidth = 6,color = "black")
-plt.title("Transition lines with tip, LLT")
-plt.show()
+plot_config("Transition lines with tip, LLT")
 
 # Plots with tip, VLM
 plt.figure()
@@ -65,10 +56,5 @@ plt.figure()
 for i in np.arange(-3,14,0.5):
     y_lst, XTrtop_lst = grab_values(f"OP_points_tip\VLM\MainWing_a={i}0_v=44.00ms.txt", 76, 1544)
     plt.plot(y_lst, np.multiply(XTrtop_lst, 0.24))
-    plt.ylim(-0.05, 0.3)
-plt.plot((0,0),(0,0.24),linewidth = 6, color = "black")
-plt.plot((0,0.64),(0.24,0.24),linewidth = 6,color = "black")
-plt.plot((0.64,0.64),(0.24,0),linewidth = 6,color = "black")
-plt.plot((0.64,0),(0.0,0.0),linewidth = 6,color = "black")
-plt.title("Transition lines with tip, VLM")
-plt.show()
+
+plot_config("Transition lines with tip, VLM")
