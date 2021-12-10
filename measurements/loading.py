b8e6003d053480b211ba32aa9ffe1253a1207d3f
Dominik Stiller <domi.stiller@gmail.com>
2021-12-10 15:27:43 +0100

    Add loading of aerodynamic data from file

---
+++ measurements/loading.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def load_pressures_from_file(filename):
+    # Need to separate loading of headers and data because there is a weird extra column at the end
+    # Load column names
+    header = (
+        pd.read_csv(f"data/cp/{filename}.txt", sep="\t", nrows=1, header=None)
+        .iloc[0]
+        .values.tolist()
+    )
+    # Remove whitespace from column names
+    header = list(map(lambda s: s.strip(), header))
+    # Load actual data
+    df = pd.read_csv(f"data/cp/{filename}.txt", sep="\t", skiprows=2, names=header).iloc[:, :-1]
+    return df
+
+
+if __name__ == "__main__":
+    df = load_pressures_from_file("2D/corr_test")
+    print(df.info())
