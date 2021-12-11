d900e6d7d64438448857a73cb511863f4bda4c5e
enesbkoca <enesbkoca@gmail.com>
2021-12-11 13:16:59 +0100

    Finalized graphs.py, added graphing commands

--- measurements/loading.py
+++ measurements/loading.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def load_pressures_from_file(filename, remove_hysteresis=False):
+def load_pressures_from_file(filename):
     # Need to separate loading of headers and data because there is a weird extra column at the end
     # Load column names
     header = (
@@ -16,12 +16,12 @@ def load_pressures_from_file(filename, remove_hysteresis=False):
         f"data/cp/{filename}.txt", sep="\t", skiprows=2, index_col=False, names=header
     ).iloc[:, 1:]
 
-    if remove_hysteresis:
-        # Ignore hysteresis part
-        idx_alpha_max = df["Alpha"].idxmax() + 1
-        df = df.iloc[:idx_alpha_max]
+    # Ignore hysteresis part
+    idx_alpha_max = df["Alpha"].idxmax() + 1
+    hysteresis = df.iloc[idx_alpha_max - 1 :]
+    df_clean = df.iloc[:idx_alpha_max]
 
-    return df
+    return df_clean, hysteresis
 
 
 if __name__ == "__main__":
